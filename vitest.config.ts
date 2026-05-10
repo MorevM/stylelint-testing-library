3954af4d7108c19919255e3bea4af0f458783f46
MorevM <max.seainside@gmail.com>
2026-05-10 04:00:26 +0300

    chore: Get rid of outdated `vitest-tsconfig-paths`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
 import { configDefaults, defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [
-		tsconfigPaths(),
-	],
+	plugins: [],
+	resolve: {
+		tsconfigPaths: true,
+	},
 	test: {
 		exclude: [
 			...configDefaults.exclude,
