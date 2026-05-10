a6395ddc731686a3516119bef4f54af431ba2e9a
MorevM <max.seainside@gmail.com>
2026-05-10 04:11:30 +0300

    ci: Use inline code for getting latest changelog entry

--- .github/extract-latest-changelog-entry.cjs
+++
@@ -1,7 +0,0 @@
-const { readFileSync } = require('node:fs');
-const { join } = require('node:path');
-
-module.exports = () => {
-	const contents = readFileSync(join(__dirname, '..', 'CHANGELOG.md'), { encoding: 'utf8' });
-	return contents.match(/## .*?\n(.*?)## [\d[]/s)[1].trim();
-};
